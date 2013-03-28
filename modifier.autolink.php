28f0135f10ef8cbbeabb825867b9f4d1b1639f0f
Linus Metzler <linus.metzler@gmail.com>
2013-03-28 17:56:38 +0100

    can now handle email addresses

--- modifier.autolink.php
+++ modifier.autolink.php
@@ -12,12 +12,13 @@ function smarty_modifier_autolink($string)
 {
 	$string = preg_replace_callback('#(?:https?://\S+)|(?:www.\S+)|(?:\S+\.\S+)#', function($arr)
 	{
+	    $email = filter_var($arr[0], FILTER_VALIDATE_EMAIL) === $arr[0] ? true : false;
+	    $origArr0 = $arr[0];
 	    if(strpos($arr[0], 'http://') !== 0 && strpos($arr[0], 'https://') !== 0)
 	    {
 	        $arr[0] = 'http://' . $arr[0];
 	    }
 	    $url = parse_url($arr[0]);
-
 	    // images
 	    if(isset($url['path'])){
 		    if(preg_match('#\.(png|jpg|gif)$#', $url['path']))
@@ -34,7 +35,11 @@ function smarty_modifier_autolink($string)
 	        return sprintf('<iframe class="embedded-video" src="http://www.youtube.com/embed/%s" allowfullscreen></iframe>', $query['v']);
 	    }
 	    //links
-	    return sprintf('<a href="%1$s">%2$s</a>', $arr[0], str_replace($url['scheme'].'://', '', $arr[0]));
+	    if($email){
+	    	return sprintf('<a href="mailto:%1$s">%1$s</a>', $origArr0);
+	    }else{
+	    	return sprintf('<a href="%1$s">%2$s</a>', $arr[0], str_replace($url['scheme'].'://', '', $arr[0]));
+	    }
 	}, $string);
     return $string;
 }
